56ca28c755e2ad4785bba74d8ecc706c7c6eae0c
h1n322 <tyvanukmax@gmail.com>
2026-09-07 00:04:32 +0300

    fix: removed deleted routers from __init__.py in python service

--- riskmate-ai-service/api/__init__.py
+++ riskmate-ai-service/api/__init__.py
@@ -1,11 +1,7 @@
-from .routes_simulations import router as simulations_router
-from .routes_portfolio import router as portfolio_router
 from .routes_market import router as market_router
 from .routes_billing import router as billing_router
 
 __all__ = [
-    "simulations_router",
-    "portfolio_router",
     "market_router",
     "billing_router",
 ]
